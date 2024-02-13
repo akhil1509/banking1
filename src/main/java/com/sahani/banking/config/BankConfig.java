41c158f1543dfe0b77d90fdfffc0397054fb9263
akhil1509 <sonusahani1995soni@gmail.com>
2024-02-14 04:37:56 +0530

    dummy

--- src/main/java/com/sahani/banking/config/BankConfig.java
+++ src/main/java/com/sahani/banking/config/BankConfig.java
@@ -9,11 +9,7 @@ import jakarta.persistence.Version;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 import java.util.Collections;
 
